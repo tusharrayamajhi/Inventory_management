55beea0195cb28d0aadca907dcb3d9c364fb12b2
Tushar Rayamajhi <tusharrayamajhi956@gmail.com>
2024-12-03 19:53:11 +0545

    upload image

--- controller/invoice.js
+++ controller/invoice.js
@@ -360,9 +360,10 @@ module.exports = async function invoice(req, res) {
         total_include_vat:0
       }
       for(let invoice of invoices){
-        totals_detals.total_amt = totals_detals.total_amt + invoice.rate
+        console.log(invoice)
+        totals_detals.total_amt = totals_detals.total_amt + parseFloat(invoice.total)
         if(invoice.vat == 1){
-          totals_detals.vatable_amt = totals_detals.vatable_amt + invoice.rate
+          totals_detals.vatable_amt = parseFloat(totals_detals.vatable_amt) +parseFloat(invoice.total)
         }
         totals_detals.vat_amt = totals_detals.vatable_amt * 13 / 100
         totals_detals.total_include_vat = totals_detals.vat_amt + totals_detals.total_amt
