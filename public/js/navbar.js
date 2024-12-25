8384d33f084e65b225d2b575ea7132c7d818e086
raajaa <sonarraja2000@gmail.com>
2024-12-25 12:35:38 +0545

    invoice edit and side bar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -6,3 +6,27 @@ document.getElementById("profile").addEventListener("click",()=>{
 
     }
 })
+
+document.getElementById("hamburger").addEventListener("click",()=>{
+document.querySelector(".sidebar_menu").style.left = 0
+})
+
+document.getElementById("hideside").addEventListener("click",()=>{
+    document.querySelector(".sidebar_menu").style.left = '-300px'
+})
+
+
+document.querySelectorAll(".features_show").forEach(features_name=>{
+    features_name.addEventListener("click",()=>{
+        if(features_name.querySelector(".features").style.display== "flex"){
+            features_name.querySelector(".features").style.display = "none";
+            features_name.querySelector(".up").style.display = "none";
+            features_name.querySelector(".down").style.display = "block";
+        }else{
+            features_name.querySelector(".features").style.display = "flex";
+            features_name.querySelector(".up").style.display = "block";
+            features_name.querySelector(".down").style.display = "none";
+    
+        }
+    })
+})
